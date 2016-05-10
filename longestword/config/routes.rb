7510bbf94cc2667dd2c5dd2ab3dab077c825b135
Yohan Belmin <ybelmin@hotmail.com>
2016-05-10 11:29:02 +0200

    routes update

--- longestword/config/routes.rb
+++ longestword/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'game' => 'longestword#game'
   get 'score' => 'longestword#score'
+  root 'longestword#game'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
